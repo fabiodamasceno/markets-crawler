fb5e69551ccb265904c0acc4ba12b5eff70a0181
Fábio Damasceno <fabio.damasceno@lambda3.com.br>
2020-05-03 19:28:32 -0300

    initial crawler code

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  semi: false,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
+  bracketSpacing: true,
+}
