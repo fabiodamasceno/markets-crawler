2ceb43ee8e54d01d663400ffa5fbe5fd032db635
Fábio Damasceno <fabio.damasceno@lambda3.com.br>
2020-05-03 19:32:47 -0300

    cleaning crawler

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-var Crawler = require("crawler");
+var Crawler = require('crawler')
 
 var c = new Crawler({
-    maxConnections : 10,
-    // This will be called for each crawled page
-    callback : function (error, res, done) {
-        if(error){
-            console.log(error);
-        }else{
-            var $ = res.$;
-            // $ is Cheerio by default
-            //a lean implementation of core jQuery designed specifically for the server
-            console.log($("title").text());
-        }
-        done();
+  maxConnections: 10,
+  // This will be called for each crawled page
+  callback: function (error, res, done) {
+    if (error) {
+      console.log(error)
+    } else {
+      var $ = res.$
+      // $ is Cheerio by default
+      //a lean implementation of core jQuery designed specifically for the server
+      console.log(res)
     }
-});
+    done()
+  },
+})
 
 // Queue just one URL, with default callback
-c.queue('http://www.google.com');
-
-// Queue a list of URLs
-
-
-// Queue some HTML code directly without grabbing (mostly for tests)
-c.queue([{
-    html: '<p>This is a <strong>test</strong></p>'c
+c.queue('http://www.google.com')
